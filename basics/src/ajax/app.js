c78765cd37427161bbf677fb2ef48a8f013596b4
Konstantin Denerz <konstantin.denerz+github@googlemail.com>
2013-02-20 00:31:19 +0100

    added notification for ajax methods

---
+++ basics/src/ajax/app.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var sleep = require('sleep');
+
+http.createServer(function (req, res) {
+  	var url = req.url;
+  	console.log(url);
+  	
+  	if(url === '/'){
+		  res.writeHead(200);
+	  	fs.readFile('index.html', function (err, data) {
+	  		res.end(data);
+		  });
+    }else if(url === '/jquery.js'){
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      fs.readFile('jquery.js', function (err, data) {
+        res.end(data);
+      });
+  	}else if(url === '/wait'){
+  		sleep.sleep(5);
+  		res.writeHead(200);
+  		res.end('sleep 5');
+  	}else{
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.end('File not found:' + url);
+	}
+}).listen(12345, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:12345/');
